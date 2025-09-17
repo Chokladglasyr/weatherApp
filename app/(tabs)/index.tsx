01c358bf3b8445ee5e7fef4ab3623048a7beeeda
Chokladglasyr <lim_ida@live.com>
2025-09-17 15:41:41 +0200

    dk

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,102 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import React from "react";
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const [city, setCity] = React.useState("stockholm");
+  const [temp, setTemp] = React.useState(0);
+  const [image, setImage] = React.useState("");
+  const [weather, setWeather] = React.useState("");
+  const [error, setError] = React.useState<Error>()
+  console.log(process.env.EXPO_PUBLIC_API_KEY)
+  const API = process.env.EXPO_PUBLIC_API_KEY 
+  console.log(API)
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const fetchWeather = async () => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`
+      );
+      const data = await res.json();
+      setTemp(data.main.temp);
+      setImage(data.weather[0].icon);
+      setWeather(data.weather[0].description);
+    } catch (err) {
+      if(err instanceof Error) {
+
+        setError(err)
+      }
+      console.log(error)
+    }
+  };
+
+  return (
+    <>
+      <SafeAreaView>
+        <View style={styles.div}>
+          <Text style={styles.titleContainer}>What is the weather atm?</Text>
+          <TextInput
+            placeholder="City"
+            style={styles.input}
+            onChangeText={setCity}
+          ></TextInput>
+          <View style={styles.button}>
+            <Button
+              color="#e36ce7ff"
+              title="Go"
+              onPress={fetchWeather}
+            ></Button>
+          </View>
+          {error && <Text>Error: {error.message}</Text>}
+          {weather && <View style={styles.div}>
+              <Text style={{textAlign: 'center'}}>Chosen city: {city}</Text>
+              <Text style={{textAlign: 'center'}}>Temperature: {temp.toFixed(2)}</Text>
+              <Text style={{textAlign: 'center'}}>Weather: {weather} </Text>
+              <Image
+                style={styles.img}
+                source={{
+                  uri: `https://openweathermap.org/img/wn/${image}@2x.png`,
+                }}
+              />
+          </View>}
+          
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
+    backgroundColor: " #272020ff",
+    color: "#000000",
+    margin: 100,
+    fontSize: 20
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    width: 280,
+    borderRadius: 10,
+    marginBottom: 50,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  div: {
+    alignItems: "center",
+    margin: 10,
+  },
+  img: {
+    height: 100,
+    width: 100,
+    backgroundColor: "#e36ce7ff",
+    alignSelf: 'center',
+    marginTop: 10,
+    borderRadius: 10
+  },
+  button: {
+    width: 100,
+    marginBottom: 20,
   },
 });
